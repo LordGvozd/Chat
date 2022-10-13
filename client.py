ce2f922fcdd03732f2c9c2fb82d2ca8e145f9b19
LordGvozd <zehapo@yandex.ru>
2022-10-13 19:51:51 +0600

    Start:

---
+++ client.py
@@ -0,0 +1,56 @@
+import socket, threading, time
+
+class Client():
+    def __init__(self, host, port, key=1111):
+        self.host = host
+        self.port = port
+        self.key = key
+
+        self.shutdown = False
+        self.join = False
+
+    def receving(self,name, sock):
+        
+        while not  self.shutdown:
+            while True:
+                data, addr = sock.recvfrom(1024)
+                print(data.decode("utf-8"))
+    def run(self, server_ip):
+        server = (socket.gethostbyname(socket.gethostname()), 7878)
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.bind((self.host, 0))
+        #s.setblocking(0)
+
+        name = input("Name: ")
+
+        rT = threading.Thread(target = self.receving, args = ("RecvThread", s))
+        rT.start()
+
+        while self.shutdown == False:
+            if (self.join == False):
+                s.sendto((("["+name+"] join chat")).encode("utf-8"), server)
+                self.join = True
+            else:
+                try:
+                    message = input(": ")
+                    if(message != ""):
+                        s.sendto((("["+name+"] => "+message)).encode("utf-8"), server)
+
+                        time.sleep(0.2)
+                except:
+                    print("Byka error - you debil")
+        rT.join()
+        s.close()
+
+
+
+
+
+
+if __name__ =="__main__":
+    client = Client(socket.gethostbyname(socket.gethostname()), 8787)
+    client.run("127.0.0.1")
+
+
+
+
---
+++ server.py
@@ -0,0 +1,51 @@
+import socket, time
+
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+        self.clients = []
+
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server.bind((self.host, self.port))
+        
+        self.run = False
+
+        print("Server init")
+
+    def start(self):
+        self.run = True
+        print("Server started")
+
+        while self.run:
+            data, addr = self.server.recvfrom(1024)
+            
+            nowtime = time.strftime("%d-%m-%Y %H:%M:%s", time.localtime())
+            
+            print("["+addr[0]+"]=["+nowtime+"]")
+            print(data.decode("utf-8"))
+
+
+            if (addr not in self.clients):
+                self.clients.append(addr)
+
+
+
+            for client in self.clients:
+                if (addr !=addr[0]):
+                    self.server.sendto(data, client)
+
+
+        server.close()
+
+
+
+
+
+    
+
+
+if __name__ == "__main__":
+    server = Server(socket.gethostbyname(socket.gethostname()), 7878)
+    server.start()
